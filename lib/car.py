03e4373aba396591e3327f6b43c9b28e94078ebd
Esther nekesa <esthernekesasi32@gmail.com>
2025-11-28 19:36:35 +0300

    Implement Vehicle/Car inheritance and method overriding

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,14 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
+    from vehicle import Vehicle  # Assuming relative import is required
+    
+  # Assuming relative import is required
+    # This class inherits __init__, wheel_size, wheel_number, and fill_up_tank() 
+    # from the Vehicle superclass.
+
+    # Overwriting the inherited go() method
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
     pass
